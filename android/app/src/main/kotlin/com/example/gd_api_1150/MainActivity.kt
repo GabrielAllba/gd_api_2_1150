8e86640524cd8967dd30759f18b9b22f7f16a4cd
rielgbrl <rielgasy9@gmail.com>
2023-11-21 13:58:45 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/gd_api_1150/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gd_api_1150
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/EditBarang.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gd_api_1150/client/BarangClient.dart';
+import 'package:gd_api_1150/entity/Barang.dart';
+import 'package:gd_api_1150/pages/Homepage.dart';
+
+class EditBarang extends StatefulWidget {
+  const EditBarang({super.key, this.id});
+
+  final int? id;
+
+  @override
+  State<EditBarang> createState() => _EditBarangState();
+}
+
+class _EditBarangState extends State<EditBarang> {
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final descController = TextEditingController();
+  final stockController = TextEditingController();
+  bool isLoading = false;
+
+  void loadData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      Barang res = await BarangClient.find(widget.id);
+      setState(() {
+        isLoading = false;
+        nameController.value = TextEditingValue(text: res.nama);
+        descController.value = TextEditingValue(text: res.deskripsi);
+        stockController.value = TextEditingValue(text: res.stok.toString());
+      });
+    } catch (err) {
+      showSnackBar(context, err.toString(), Colors.red);
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.id != null) {
+      loadData();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void onSubmit() async {
+      if (!_formKey.currentState!.validate()) return;
+
+      Barang input = Barang(
+        id: widget.id ?? 0,
+        nama: nameController.text,
+        deskripsi: descController.text,
+        stok: int.parse(stockController.text),
+      );
+
+      try {
+        if (widget.id == null) {
+          await BarangClient.create(input);
+        } else {
+          await BarangClient.update(input);
+        }
+        showSnackBar(context, 'Success', Colors.green);
+        Navigator.pop(context);
+      } catch (err) {
+        showSnackBar(context, err.toString(), Colors.red);
+        Navigator.pop(context);
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.id == null ? 'Tambah Barang' : 'Edit Barang',
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 10,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Masukkan nama',
+                        ),
+                        controller: nameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Field Required';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 10,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Masukkan Deskripsi',
+                        ),
+                        controller: descController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Field Required';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 10,
+                      ),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Masukkan Stok',
+                        ),
+                        controller: stockController,
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 10,
+                      ),
+                      child: ElevatedButton(
+                        onPressed: onSubmit,
+                        child: Text(
+                          widget.id == null ? 'Tambah' : 'Edit',
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+      ),
+    );
+  }
+
+  void showSnackBar(BuildContext context, String msg, Color bg) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: bg,
+        action: SnackBarAction(
+          label: 'hide',
+          onPressed: scaffold.hideCurrentSnackBar,
+        ),
+      ),
+    );
+  }
+}
